5e283e1ab1599222d0b54b641d995210ed0e3859
Suebin Kim <k.sue3028@gmail.com>
2024-01-26 16:42:34 +0900

    feat: init api instance

--- lookncook/lib/main.dart
+++ lookncook/lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/route_manager.dart';
 import 'dart:async';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lookncook/apis/API.dart';
 import 'package:lookncook/screens/home_screen.dart';
 
-void main() {
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  Timer(const Duration(seconds: 2), () async {
+    await dotenv.load(fileName: ".env");
+    await API.instance.init();
+    FlutterNativeSplash.remove();
+  });
   runApp(const MyApp());
 }
 
@@ -17,9 +23,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 2), () {
-      FlutterNativeSplash.remove();
-    });
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(

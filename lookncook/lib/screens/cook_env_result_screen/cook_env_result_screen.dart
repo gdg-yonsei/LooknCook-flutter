d798360ad8ba9797edd7a2e3325a659824285ffb
Suebin Kim <k.sue3028@gmail.com>
2024-02-14 03:54:20 +0900

    feat: pass recipe prop from cook env screen

--- lookncook/lib/screens/cook_env_result_screen/cook_env_result_screen.dart
+++ lookncook/lib/screens/cook_env_result_screen/cook_env_result_screen.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:lookncook/dtos/cook_env_state.dart';
+import 'package:lookncook/dtos/recipe.dart';
 import 'package:lookncook/screens/cook_env_result_screen/components/cook_env_state_item.dart';
+import 'package:lookncook/screens/cooking_screen/cooking_screen.dart';
 
 class CookEnvResultScreen extends StatefulWidget {
   final List<CookEnvState> cookEnvStateList;
+  final Recipe recipe;
   final String imageUrl;
 
   const CookEnvResultScreen(
-      {super.key, required this.cookEnvStateList, required this.imageUrl});
+      {super.key,
+      required this.cookEnvStateList,
+      required this.imageUrl,
+      required this.recipe});
 
   @override
   State<CookEnvResultScreen> createState() => _CookEnvResultScreenState();
@@ -24,12 +30,9 @@ class _CookEnvResultScreenState extends State<CookEnvResultScreen> {
       widget.cookEnvStateList.map((i) => i.content).join("....."),
     );
 
-    // TODO(용재): 요리하기 화면으로 넘어가기
-    // Get.to(() => CookEnvResultScreen(
-    //       cookEnvStateList: dummyCookEnvState,
-    //       imageUrl:
-    //           'https://www.world-grain.com/ext/resources/2022/10/25/Rice_AdobeStock_64819529_E.jpg?height=667&t=1666706505&width=1080',
-    //     ));
+    Get.to(() => CookingScreen(
+          recipe: widget.recipe,
+        ));
   }
 
   @override

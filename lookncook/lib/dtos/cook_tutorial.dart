92229483b531fd16ed1b244b9fec94673dde44ed
Suebin Kim <k.sue3028@gmail.com>
2024-02-17 21:37:17 +0900

    feat: make cook tutorial dto

---
+++ lookncook/lib/dtos/cook_tutorial.dart
@@ -0,0 +1,12 @@
+class CookTutorial {
+  final String title;
+  final String description;
+  final String thumbnail;
+  final String url;
+
+  CookTutorial(
+      {required this.title,
+      required this.description,
+      required this.thumbnail,
+      required this.url});
+}

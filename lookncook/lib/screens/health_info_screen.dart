0f86d78bece86e2d2b30057ddd688f42b04cc4f6
Suebin Kim <k.sue3028@gmail.com>
2024-02-25 05:42:15 +0900

    feat: make health info screen

--- lookncook/lib/screens/health_info_screen.dart
+++ lookncook/lib/screens/health_info_screen.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart'; // Import 'provider' if using it
 
 // Example State Management class (using Provider)
 class userInputData with ChangeNotifier {
-  int _age = 0;
+  int _age = 45;
   List<String> _selectedDiseases = [];
 
   int get age => _age;
   List<String> get selectedDiseases => _selectedDiseases;
 
-  void setAge(int age) {
-    _age = age;
+  void incrementAge() {
+    _age++;
+    notifyListeners();
+  }
+
+  void decrementAge() {
+    _age--;
     notifyListeners();
   }
 
@@ -53,11 +59,21 @@ class _HealthInfoScreenState extends State<HealthInfoScreen> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                _buildAgeInput(),
-                _buildMedicalHistoryInput(),
-                const SizedBox(height: 30), // Add spacing
-                _buildNextButton(),
+                Column(
+                  children: [
+                    _buildAgeInput(),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    _buildMedicalHistoryInput(),
+                  ],
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                _buildNextButton(context.textTheme),
               ],
             ),
           ),
@@ -70,11 +86,53 @@ class _HealthInfoScreenState extends State<HealthInfoScreen> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('Age'),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            "Age",
+            style: context.textTheme.headlineMedium,
+          ),
+        ),
         Consumer<userInputData>(builder: (context, data, child) {
-          return InputDecorator(
-            decoration: const InputDecoration(border: OutlineInputBorder()),
-            child: Text(data.age.toString()),
+          return Semantics(
+            container: true,
+            label: 'Age Picker. Current age is ${data.age}',
+            value: (data.age).toString(),
+            increasedValue: (data.age + 1).toString(),
+            decreasedValue: (data.age - 1).toString(),
+            onIncrease: () => setState(() => data.incrementAge()),
+            onDecrease: () => setState(() => data.decrementAge()),
+            child: Column(
+              children: [
+                Text(
+                  '${data.age}',
+                  style: const TextStyle(fontSize: 36),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Semantics(
+                      onTapHint: 'Decrease Age',
+                      button: true,
+                      child: ElevatedButton(
+                        onPressed: () => setState(data.decrementAge),
+                        child: const Text('-'),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Semantics(
+                      onTapHint: 'Increase Age',
+                      button: true,
+                      child: ElevatedButton(
+                        onPressed: () => setState(data.incrementAge),
+                        child: const Text('+'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         }),
         // Add a dial picker (package needed) or a numerical input field
@@ -86,35 +144,57 @@ class _HealthInfoScreenState extends State<HealthInfoScreen> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('Medical History'),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            "Medical History",
+            style: context.textTheme.headlineMedium,
+          ),
+        ),
         Consumer<userInputData>(builder: (context, data, child) {
-          return Wrap(
-            spacing: 8.0,
-            children: _diseases.map((disease) {
-              return ChoiceChip(
-                label: Text(disease),
-                selected: data.selectedDiseases.contains(disease),
-                onSelected: (isSelected) => data.toggleDisease(disease),
-              );
-            }).toList(),
+          return Semantics(
+            label: 'Medical history. Select your previous conditions',
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: _diseases.map((disease) {
+                return ChoiceChip(
+                  label: Text(disease),
+                  selected: data.selectedDiseases.contains(disease),
+                  onSelected: (isSelected) => data.toggleDisease(disease),
+                );
+              }).toList(),
+            ),
           );
         }),
       ],
     );
   }
 
-  Widget _buildNextButton() {
+  Widget _buildNextButton(TextTheme textTheme) {
     return Consumer<userInputData>(builder: (context, data, child) {
-      return SizedBox(
-        width: double.infinity, // Button takes full width
-        child: ElevatedButton(
-          onPressed: (data.age != 0 && data.selectedDiseases.isNotEmpty)
-              ? () {
-                  // Navigate to next screen
-                }
-              : null,
-          child: const Text('Next'),
-        ),
+      return Semantics(
+        label: "Next Button",
+        button: true, // Indicate it's a button
+        enabled: (data.age != 0 && data.selectedDiseases.isNotEmpty),
+        onTapHint: 'Tap to proceed to the next screen', // Ac
+        child: SizedBox(
+            width: double.infinity, // Button takes full width
+            child: ElevatedButton(
+              onPressed: (data.age != 0 && data.selectedDiseases.isNotEmpty)
+                  ? () {
+                      // Navigate to next screen
+                    }
+                  : null,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  "Next",
+                  style: textTheme.titleLarge?.apply(fontWeightDelta: 2),
+                  // style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )),
       );
     });
   }
